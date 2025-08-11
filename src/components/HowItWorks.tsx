4351c9e34fb9e2073ab0231cb9c99101022a2670
jayrajak-ui <jayrajak@ekasupport.com>
2025-08-11 15:08:27 +0530

    114

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -25,7 +25,7 @@ export const HowItWorks = () => {
   return (
     <section className="bg-slate-50 py-20 px-4">
       <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-bold font-serif mb-2 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
+        <h2 className="text-4xl font-bold font-serif mb-2 text-neutral-800">
           Eka Scribe
         </h2>
         <p className="text-xl text-neutral-600">
